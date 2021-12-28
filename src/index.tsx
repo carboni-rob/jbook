215d9edc8e567ff32bdd562b32b7c1204ecaca37
carboni-rob <roberto.carboni@laduma.com>
2021-12-28 16:34:55 +0000

    Extract CodeCell component

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,11 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import { useState } from "react";
 import ReactDOM from "react-dom";
-
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
-import bundle from "./bundler";
+import CodeCell from "./components/code-cell";
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="// Import libraries, stylesheets and other resources or write some code here"
-        onChange={(value: string) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
